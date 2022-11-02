29838d290815fdb2e0bc8d48ebcf63b6953aeea6
devgru <askeraubakirov15@gmail.com>
2022-11-03 00:27:25 +0600

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
       host: '0.0.0.0',
    },
     axios: {
-    baseURL: 'https://185.22.67.15:443',
+    baseURL: 'https://serikaubakirov.tk',
   },
   head: {
     titleTemplate: '%s - serik.aubakirov',
